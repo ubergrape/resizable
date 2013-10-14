30f9736d776df72fe3b5f979ee44fbfc5e20ec4d
jheytompkins <jhey.tompkins@gmail.com>
2013-10-14 22:24:29 +0100

    Added ghosting and set directions.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = resizable;
 
 function resizable(element, options) {
 	if (!(this instanceof resizable)) return new resizable(element, options);
-	this.defaults = {
+	this._defaults = {
 		handles: ['north', 'south', 'west', 'east', 'southeast', 'southwest', 'northeast', 'northwest']
 	};
 	this.element = element;
@@ -14,74 +14,89 @@ function resizable(element, options) {
 		}
 		return a;
 	}
-	this.options = extend(this.defaults, options);
-	this.startX;
-	this.startY;
-	this.startW;
-	this.startH;
+	this._options = extend(this._defaults, options);
+	this._handles = this._options.handles;
+	this._ghosting = (this._options.ghosting !== undefined) ? this._options.ghosting : false; 
 	this._create();
+}
+resizable.prototype.setDirections = function (handles) {
+	var resizable = this,
+		currentDirections = resizable._handles;
+	if (handles === undefined) {
+		handles = resizable._defaults.handles;
 	}
-resizable.prototype._create = function () {
-	//basically adding the handles in which makes sense.
-	var resizable = this;
-	resizable.element.className = resizable.element.className + ' resizable';
-	[].forEach.call(resizable.options.handles, function (handle) {
-		//create a div and append it to my resizable.
-		var handleEl = document.createElement('div');
-		handleEl.className = handleEl.className + ' resize-handle resize-handle-' + handle;
-		handleEl.setAttribute('data-resize-direction', handle);
-		var resizeMove = function (e) {
-			switch (handle) {
-				case 'north':
-					resizable.element.style.top = e.pageY + "px";
-					resizable.element.style.height = (resizable.startH + (resizable.startY - e.pageY)) + "px";
-					break;
-				case 'south':
-					resizable.element.style.height = (resizable.startH + (e.pageY - resizable.startY)) + "px";
-					break;
-				case 'east':
-					resizable.element.style.width = (resizable.startW + (e.pageX - resizable.startX)) + "px";
-					break;	
-				case 'west':
-					resizable.element.style.left = e.pageX + "px";
-					resizable.element.style.width = (resizable.startW + (resizable.startX - e.pageX)) + "px";
-					break;
-				case 'northeast':
-					resizable.element.style.top = e.pageY + "px";
-					resizable.element.style.height = (resizable.startH + (resizable.startY - e.pageY)) + "px";
-					resizable.element.style.width = (resizable.startW + (e.pageX - resizable.startX)) + "px";
-					break;
-				case 'northwest':
-					resizable.element.style.top = e.pageY + "px";
-					resizable.element.style.height = (resizable.startH + (resizable.startY - e.pageY)) + "px";
-					resizable.element.style.left = e.pageX + "px";
-					resizable.element.style.width = (resizable.startW + (resizable.startX - e.pageX)) + "px";
-					break;
-				case 'southeast':
-					resizable.element.style.height = (resizable.startH + (e.pageY - resizable.startY)) + "px";
-					resizable.element.style.width = (resizable.startW + (e.pageX - resizable.startX)) + "px";
-					break;
-				case 'southwest':
-					resizable.element.style.height = (resizable.startH + (e.pageY - resizable.startY)) + "px";
-					resizable.element.style.left = e.pageX + "px";
-					resizable.element.style.width = (resizable.startW + (resizable.startX - e.pageX)) + "px";
-					break;
+	resizable._handles = handles;
+	[].forEach.call(currentDirections, function (handle) {
+		if (handles.indexOf(handle) === -1) {
+			resizable.element.querySelector('.rsize-d-' + handle).style.display = 'none';
+		}
+	});
+	[].forEach.call(resizable._handles, function (handle) {	
+		if (resizable.element.querySelector('.rsize-d-' + handle) !== undefined) {
+			resizable.element.querySelector('.rsize-d-' + handle).style.display = 'block';
+		} else {
+			resizable._createHandle(handle);
+		}
+	});
+}
+resizable.prototype._createHandle = function (handle) {
+	var resizable = this,
+		rh = document.createElement('div'),
+		ghost,
+		resize = function (e) {
+			if (handle.indexOf('north') !== -1) {
+				resizable.element.style.top = e.pageY + "px";
+				resizable.element.style.height = (resizable._startH + (resizable._startY - e.pageY)) + "px";
+			}
+			if (handle.indexOf('south') !== -1) {
+				resizable.element.style.height = (resizable._startH + (e.pageY - resizable._startY)) + "px";
+			}
+			if (handle.indexOf('east') !== -1) {
+				resizable.element.style.width = (resizable._startW + (e.pageX - resizable._startX)) + "px";
+			}
+			if (handle.indexOf('west') !== -1) {
+				resizable.element.style.left = e.pageX + "px";
+				resizable.element.style.width = (resizable._startW + (resizable._startX - e.pageX)) + "px";
+			}
+		},
+		stop = function () {
+			if (resizable._ghosting) {
+				[].forEach.call(resizable.element.parentNode.querySelectorAll('.ghost'), function (ghost) {
+					ghost.remove();
+				});
+			}
+			window.removeEventListener("mousemove", resize, true);
+		},
+		start = function (e) {
+			e.stopPropagation();
+			resizable._startX = e.pageX;
+			resizable._startY = e.pageY;
+			resizable._startW = resizable.element.offsetWidth;
+			resizable._startH = resizable.element.offsetHeight;	
+			window.addEventListener("mousemove", resize, true);
+			window.addEventListener("mouseup", stop, true);
+			if (resizable._ghosting) {
+				ghost = document.createElement('div');
+				resizable.element.parentNode.appendChild(ghost);
+				ghost.className = 'ghost';
+				ghost.style.border = '1px dashed #000';
+				ghost.style.background = 'transparent';
+				ghost.style.position = 'absolute';
+				ghost.style.width = resizable.element.offsetWidth - 2 + 'px';
+				ghost.style.height = resizable.element.offsetHeight - 2 + 'px';
+				ghost.style.left = resizable.element.offsetLeft + 'px';
+				ghost.style.top = resizable.element.offsetTop + 'px';
 			}
 		};
-		var mouseUp = function (event) {
-			window.removeEventListener("mousemove", resizeMove, true);
-		}
-		var mouseDown = function (event) {
-			event.stopPropagation();
-			resizable.startX = event.pageX;
-			resizable.startY = event.pageY;
-			resizable.style = window.getComputedStyle(resizable.element);
-			resizable.startW = parseInt(resizable.style.width, 10);
-			resizable.startH = parseInt(resizable.style.height, 10);	
-			window.addEventListener("mousemove", resizeMove, true);
-			window.addEventListener("mouseup", mouseUp, true);	
-		}
-		resizable.element.appendChild(handleEl);
-		handleEl.addEventListener("mousedown", mouseDown, false);
+	rh.className = rh.className + ' rsize-d rsize-d-' + handle;
+	rh.setAttribute('data-rsize-d', handle);
+	resizable.element.appendChild(rh);
+	rh.addEventListener("mousedown", start, false);
+}
+resizable.prototype._create = function () {
+	var resizable = this;
+	resizable.element.className = resizable.element.className + ' rsizable';
+	[].forEach.call(resizable._options.handles, function (handle) {
+		resizable._createHandle(handle);
 	});
 }
